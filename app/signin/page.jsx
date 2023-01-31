e2fecebfc589bb264d5f4e500c9e0062fd2763f4
ben519 <bgorman519@gmail.com>
2023-01-31 09:30:08 -0600

    add required attribute to form fields

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -3,16 +3,16 @@ export default function SignIn() {
     <main>
       <h1>Sign in</h1>
 
-      <form>
+      <form id="signin_form">
 
         <div>
           <label htmlFor="signin_email">Email</label>
-          <input type="email" id="signin_email" />
+          <input type="email" id="signin_email" required />
         </div>
 
         <div>
           <label htmlFor="signin_password">Password</label>
-          <input type="password" id="signin_password" />
+          <input type="password" id="signin_password" required />
         </div>
 
         <button type="submit">Sign up</button>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -3,26 +3,26 @@ export default function SignUp() {
     <main>
       <h1>Sign up</h1>
 
-      <form>
+      <form id="signup_form">
 
         <div>
           <label htmlFor="signup_full_name">Full Name</label>
-          <input type="text" id="signup_email" />
+          <input type="text" id="signup_email" required />
         </div>
 
         <div>
           <label htmlFor="signup_email">Email</label>
-          <input type="email" id="signup_email" />
+          <input type="email" id="signup_email" required />
         </div>
 
         <div>
           <label htmlFor="signup_password">Password</label>
-          <input type="password" id="signup_password" />
+          <input type="password" id="signup_password" required />
         </div>
 
         <div>
           <label htmlFor="signup_confirm_password">Confirm password</label>
-          <input type="password" id="signup_confirm_password" />
+          <input type="password" id="signup_confirm_password" required />
         </div>
 
         <button type="submit">Sign up</button>
