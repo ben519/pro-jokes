b80b00617938f00ee4ed2b8c200b4badb2e14534
ben519 <bgorman519@gmail.com>
2023-01-31 14:25:28 -0600

    instantiate firebase app

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvcsGtWpYZdcJ5UBGKRRSzjPGgmUYEzb0",
+  authDomain: "pro-jokes.firebaseapp.com",
+  projectId: "pro-jokes",
+  storageBucket: "pro-jokes.appspot.com",
+  messagingSenderId: "1053541595629",
+  appId: "1:1053541595629:web:ec2c865aa932601b9b4fe4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
