e7da090bff56d882ede9a01ab925ccd098474fe4
ben519 <bgorman519@gmail.com>
2023-01-31 13:17:32 -0600

    set up controlled inputs

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,4 +1,24 @@
+'use client';
+
+import { useState } from "react";
+
 export default function SignIn() {
+  const [formData, setFormData] = useState({
+    'email': '',
+    'password': ''
+  });
+
+  function onChangeHandler(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setFormData(prevFormData => (
+      {
+        ...prevFormData,
+        [key]: value
+      }
+    ))
+  }
+
   return (
     <main>
       <h1>Sign in</h1>
@@ -7,12 +27,26 @@ export default function SignIn() {
 
         <div>
           <label htmlFor="signin_email">Email</label>
-          <input type="email" id="signin_email" required />
+          <input
+            type="email"
+            id="signin_email"
+            name="email"
+            required
+            value={ formData.email }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <div>
           <label htmlFor="signin_password">Password</label>
-          <input type="password" id="signin_password" required />
+          <input
+            type="password"
+            id="signin_password"
+            name="password"
+            required
+            value={ formData.password }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <button type="submit">Sign up</button>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,4 +1,26 @@
+'use client';
+
+import { useState } from "react";
+
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    'fullName': '',
+    'email': '',
+    'password': '',
+    'confirmPassword': '',
+  });
+
+  function onChangeHandler(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setFormData(prevFormData => (
+      {
+        ...prevFormData,
+        [key]: value
+      }
+    ))
+  }
+
   return (
     <main>
       <h1>Sign up</h1>
@@ -7,22 +29,50 @@ export default function SignUp() {
 
         <div>
           <label htmlFor="signup_full_name">Full Name</label>
-          <input type="text" id="signup_email" required />
+          <input
+            type="text"
+            id="signup_full_name"
+            name="fullName"
+            required
+            value={ formData.fullName }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <div>
           <label htmlFor="signup_email">Email</label>
-          <input type="email" id="signup_email" required />
+          <input
+            type="email"
+            id="signup_email"
+            name="email"
+            required
+            value={ formData.email }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <div>
           <label htmlFor="signup_password">Password</label>
-          <input type="password" id="signup_password" required />
+          <input
+            type="password"
+            id="signup_password"
+            name="password"
+            required
+            value={ formData.password }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <div>
           <label htmlFor="signup_confirm_password">Confirm password</label>
-          <input type="password" id="signup_confirm_password" required />
+          <input
+            type="password"
+            id="signup_confirm_password"
+            name="confirmPassword"
+            required
+            value={ formData.confirmPassword }
+            onChange={ onChangeHandler }
+          />
         </div>
 
         <button type="submit">Sign up</button>
