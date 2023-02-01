e529e519ce796221a8797f60fa60dbe058b53eab
ben519 <bgorman519@gmail.com>
2023-02-01 13:49:28 -0600

    display the logged in user

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,12 +1,39 @@
+'use client';
+
 import Link from "next/link";
+import { auth } from "@/firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 
 export default function Navbar() {
+    const [user, setUser] = useState(undefined);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log("=== onAuthStateChanged() invoked ============");
+
+            if (currentUser) {
+                // User signed in
+                console.log("currentUser", currentUser)
+                setUser(currentUser);
+            } else {
+                // User signed out
+                setUser(undefined);
+            }
+        });
+
+        // unsubscribe from the listener when the component unmounts
+        return unsubscribe;
+    }, []);
+
     return (
         <nav>
-            <Link href="/">Home</Link> |&nbsp;
-            <Link href="/signup">Sign up</Link> |&nbsp;
-            <Link href="/signin">Sign in</Link>
+            <Link href="/">Home</Link>
+            { !user && <>&nbsp;| <Link href="/signup">Sign up</Link></> }
+            { !user && <>&nbsp;| <Link href="/signin">Sign in</Link></> }
+            { user && <>&nbsp;| <Link href="#">Sign out</Link></> }
+            { user && <>&nbsp;| { user.email }</> }
         </nav>
     )
 }
